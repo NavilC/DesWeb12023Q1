44ffcc6e8a69c9e4eddd6ffc04b1f0778be889e7
frlopez88 <68876977+frlopez88@users.noreply.github.com>
2023-01-18 19:26:26 -0600

    Commit Final de Clase

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 var http = require('http');
+var mismodulos = require('./mismodulos');
 
 http.createServer( function(req, res){
 
     res.writeHead(200, {'Content-Type': 'text/plain'});
-    res.end("Hola este es un cambio");
+    res.write('La suma es :' + mismodulos.suma('1', '2'));
+    res.write(' ');
+    res.end( 'Mi fecha de cumple es: ' +  mismodulos.miFecha());
 
 }).listen(3000);
