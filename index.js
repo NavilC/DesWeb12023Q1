6d84f37e464a920b14a75b97fe35924fb2cf1484
frlopez88 <68876977+frlopez88@users.noreply.github.com>
2023-01-18 19:04:41 -0600

    Commit de cambio prueba

--- index.js
+++ index.js
@@ -3,6 +3,6 @@ var http = require('http');
 http.createServer( function(req, res){
 
     res.writeHead(200, {'Content-Type': 'text/plain'});
-    res.end("Hola este es mi primer programa en node");
+    res.end("Hola este es un cambio");
 
 }).listen(8080);
