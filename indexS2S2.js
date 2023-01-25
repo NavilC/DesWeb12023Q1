79ee46cea6ab263d5aad803150d7748a75d44059
frlopez88 <68876977+frlopez88@users.noreply.github.com>
2023-01-24 18:36:04 -0600

    Servidor Multiples Metodos 1 Tabla

---
+++ indexS2S2.js
@@ -0,0 +1,64 @@
+var mysql =  require('mysql');
+var http = require('http');
+var url = require('url');
+
+http.createServer(function(req, res){
+
+    res.writeHead(200, {'Content-Type': 'text/json'});
+    var q = url.parse(req.url, true);
+    var datos = q.query; 
+
+    var accion = datos.accion;
+
+    let con = mysql.createConnection({
+        host: "127.0.0.1",
+        user: "root",
+        password: "passa1234b",
+        database: "deswebq12023"
+    });
+
+    let sql="";
+    let parametros = [];
+
+    switch (accion){
+
+        case "insert":
+            sql = "insert into tbl_persona "+
+            " ( nombre_persona, apellido_persona, fecha_nacimiento) "+
+            " values  "+
+            " (?, ?, ?) ";
+            parametros = [ datos.nombre, datos.apellido, datos.fecha_nacimiento ];
+            break;
+        case "select":
+            sql = "select * from tbl_persona ";
+            break;
+        
+        default:
+            break;
+
+    }
+
+    
+    con.connect(function(err){
+
+        if (err){
+            console.log(err);
+        }else{
+
+            con.query( sql,parametros, function(err, result){
+
+                if (err){
+                    console.log(err);
+
+                }else{
+                    res.write( JSON.stringify(result) );
+                    res.end();
+                }
+
+            } );
+
+        }
+
+    });
+
+}).listen(3000);
