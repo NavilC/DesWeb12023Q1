c287d4fe556f6036daffc7ef25655f860037d8a9
frlopez88 <68876977+frlopez88@users.noreply.github.com>
2023-01-25 18:49:20 -0600

    Web Service CRUD Metodo GET

--- indexS2S2.js
+++ indexS2S2.js
@@ -20,45 +20,113 @@ http.createServer(function(req, res){
     let sql="";
     let parametros = [];
 
-    switch (accion){
+    let tabla = datos.tabla;
 
-        case "insert":
-            sql = "insert into tbl_persona "+
-            " ( nombre_persona, apellido_persona, fecha_nacimiento) "+
-            " values  "+
-            " (?, ?, ?) ";
-            parametros = [ datos.nombre, datos.apellido, datos.fecha_nacimiento ];
-            break;
-        case "select":
-            sql = "select * from tbl_persona ";
-            break;
-        
-        default:
-            break;
+    if (tabla == "Persona"){
+        switch (accion){
+
+            case "insert":
+                sql = "insert into tbl_persona "+
+                " ( nombre_persona, apellido_persona, fecha_nacimiento) "+
+                " values  "+
+                " (?, ?, ?) ";
+                parametros = [  datos.nombre, 
+                                datos.apellido, 
+                                datos.fecha_nacimiento ];
+                break;
+            case "select":
+                sql = "select * from tbl_persona ";
+                break;
+            case "update":
+
+                sql = " update tbl_persona "+
+                        " set  nombre_persona = ? ,  "+
+                                " apellido_persona = ? , " +
+                                " fecha_nacimiento = ? "+
+                                " where id_persona = ? ";
+                parametros = [  datos.nombre, 
+                                datos.apellido, 
+                                datos.fecha_nacimiento,
+                                datos.id_persona  ];
+
+                break;
+            case "delete":
+                sql = "delete from tbl_persona where id_persona = ?"
+                parametros = [ datos.id_persona ];
+                break;
+            default:
+                sql = "";
+                break;
+
+        }
+    }
+
+    if (tabla == "Telefono"){
+
+        switch (accion){
+            case "select":
+                sql = "select * from tbl_telefonos";
+                break;
+            case "insert":
+                sql = "insert into tbl_telefonos "+ 
+                    " ( numero, id_persona) " +
+                    " values "+
+                    " ( ?, ?) ";
+                parametros = [ datos.numero, datos.id_persona ];
+                break;
+            
+            case "update":
+                sql = " update  tbl_telefonos "+
+                        " set numero = ? , id_persona = ? "+
+                        " where id_telefono = ? ";
+                parametros = [  datos.numero, 
+                                datos.id_persona, 
+                                datos.id_telefono ];  
+            case "delete":
+                sql = "delete from tbl_telefonos where id_telefono = ?";
+                parametros = [datos.id_telefono ];
+                break;
+            default:
+                break;
+
+        }
+
+    }
+    
+
+    if (sql != ""){
+
+        con.connect(function(err){
+
+            if (err){
+                console.log(err);
+            }else{
+    
+                con.query( sql,parametros, function(err, result){
+    
+                    if (err){
+                        console.log(err);
+    
+                    }else{
+                        res.write( JSON.stringify(result) );
+                        res.end();
+                    }
+    
+                } );
+    
+            }
+    
+        });
+
+    }else{
+
+        let retorno = { mensaje : "Metodo no encontrado" };
+
+        res.write( JSON.stringify(retorno) );
+        res.end();
 
     }
 
     
-    con.connect(function(err){
-
-        if (err){
-            console.log(err);
-        }else{
-
-            con.query( sql,parametros, function(err, result){
-
-                if (err){
-                    console.log(err);
-
-                }else{
-                    res.write( JSON.stringify(result) );
-                    res.end();
-                }
-
-            } );
-
-        }
-
-    });
 
 }).listen(3000);
